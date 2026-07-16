a2e7fb507d2aa5033e40ed56e452cae260f96510
MrAraaaaaa <2206062@itg.ac.id>
2026-07-16 15:33:04 +0700

    Fix: not secure

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- Pastikan ini di-import
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Paksa menggunakan HTTPS jika di lingkungan production/vercel
+        if (config('app.env') === 'production' || env('VERCEL') === '1') {
+            URL::forceScheme('https');
+        }
     }
 }
