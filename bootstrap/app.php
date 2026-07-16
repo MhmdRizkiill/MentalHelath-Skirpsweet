178b6334f6e8f6d7c3ba579d3bfc06343e33bfb5
MrAraaaaaa <2206062@itg.ac.id>
2026-07-16 16:30:20 +0700

    Fix: error login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->trustProxies(at: '*'); // <-- Pastikan penulisannya persis begini
+    $middleware->trustProxies(at: '*');
+
+    // Tambahkan baris alias di bawah ini:
+    $middleware->alias([
+        'role' => \App\Http\Middleware\RoleMiddleware::class, // <-- Sesuaikan dengan nama file middleware Anda
+    ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
